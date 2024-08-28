d74578977d60db384f5323362c3389bbc3b3d01c
Aidan-B1409 <aidanb1049@gmail.com>
2024-08-27 21:03:00 -0700

    Change prediction model type to Json

--- src/models/prediction.py
+++ src/models/prediction.py
@@ -1,10 +1,12 @@
+from typing import Any
+
 import numpy as np
-from pydantic import BaseModel
+from pydantic import BaseModel, Json
 
 
 class PredictionResult(BaseModel):
     img_seg: np.ndarray
-    results: dict
+    results: Json[Any]
 
     class Config:
         arbitrary_types_allowed = True
